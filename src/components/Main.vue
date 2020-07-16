dea6be5907d9b04fd7583b4b0d607a621b8ea881
leeokhee <pixellog@gmail.com>
2020-07-16 13:44:54 +0900

    맵 버튼 추가

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -19,9 +19,22 @@
 
             <section class="shadow-lg rounded-lg">
                 <b-row no-gutters>
-                    <b-col cols="10" class="rounded-left position-relative overflow-hidden bg-secondary" style="height: calc(100vh - 90px)">
+                    <b-col cols="10" class="rounded-left position-relative overflow-hidden bg-secondary" style="height: calc(100vh - 88px)">
                         <!-- 지도영역 -->
                         <img src="/assets/images/map-bg.png" alt="" style="position:absolute; width: 100%; height: 100%; border-radius: 10px 0 0 10px;">
+
+                        <!-- controller -->
+                        <div style="position:absolute; right: 10px;bottom: 10px; z-index: 1000">
+                            <b-button size="sm" variant="light" class="shadow" block>
+                                <b-icon font-scale="1.2" icon="cursor-fill"/>
+                            </b-button>
+                            <b-button size="sm" variant="light" class="shadow" block>
+                                <b-icon font-scale="1.2" icon="plus"/>
+                            </b-button>
+                            <b-button size="sm" variant="light" class="shadow" style="margin-top: 1px;">
+                                <b-icon font-scale="1.2" icon="dash"/>
+                            </b-button>
+                        </div>
 
                         <div class="position-absolute w-100 h-100">
 
@@ -53,8 +66,16 @@
                     <b-col cols="2" class="px-3 pt-3">
 
                         <div class="position-relative">
-                            <b-input type="search" class="rounded-lg" @focus="search = true"></b-input>
-                            <div class="border p-3 rounded-lg position-absolute w-100 bg-white" style="top: 38px; z-index: 9999" v-if="search">
+                            <b-input-group class="ml-auto border rounded">
+                                <b-form-input type="search" @focus="search = true" class="border-0"></b-form-input>
+                                <b-input-group-append>
+                                    <b-button @click="search = false" class="text-secondary bg-white border-0">
+                                        <b-icon font-scale="1.2" icon="search"/>
+                                    </b-button>
+                                </b-input-group-append>
+                            </b-input-group>
+
+                            <div class="border p-3 rounded-lg position-absolute w-100 bg-white" style="top: 40px; z-index: 9999" v-if="search">
                                 <div class="text-primary d-flex font-weight-bold">검색 결과[5개]
                                     <b-link class="ml-auto" @click="search = false">
                                         <b-icon font-scale="1.5" icon="x"/>
